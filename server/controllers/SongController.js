309fa766b444901a55394c5af9dc0f65716d6fa7
Nick Tse <nicholashtse@gmail.com>
2023-01-15 22:08:46 -0800

    implement post route in SongController to add a song to user's 'likedSongs' list

--- server/controllers/SongController.js
+++ server/controllers/SongController.js
@@ -1,4 +1,5 @@
 const Song = require("../models/Song");
+const User = require("../models/User");
 
 exports.get_showAllSongs = async (req, res) => {
   try {
@@ -14,8 +15,27 @@ exports.get_showAllSongs = async (req, res) => {
 
 // put request to that song for when you want to like a song
 exports.post_addLikedSong = async (req, res) => {
+  const idToFind = "63c36fd82a88d67c45b83e80";
   try {
-    console.log(req.body);
+    // console.log(req.body);
+    let user1 = await User.findById(idToFind);
+    // console.log("user1.likedSongs:", user1.likedSongs);
+
+    let songToAdd = await Song.findById(req.body.songId);
+    // console.log("songToAdd:", songToAdd);
+    // console.log("usersLikedSongs:", user1.likedSongs);
+    user1.likedSongs.push(songToAdd);
+    // const userToUpdate = await User.findByIdAndUpdate('63c36fd82a88d67c45b83e80', { likedSongs: likedSongs.push(Song.findById(req.body))}, {new: true}); 
+    // User.findOneAndUpdate({ _id: "63c36fd82a88d67c45b83e80"}, {likedSongs: userLikedSongs});
+    // User.updateOne({ username: "user1" }, { $set: { likedSongs: usersLikedSongs}});
+    // User.updateOne({ username: "user1" }, { likedSongs: user1.likedSongs}
+    User.updateOne({ _id: idToFind }, { likedSongs: user1.likedSongs}, function (err, docs) {
+      if (err){
+        console.log(err);
+      } else {
+        console.log("updated docs : " , docs)
+      }
+    });
   } catch(e) {
     console.log("error:", e);
   }
